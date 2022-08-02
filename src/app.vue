e8d3fbfe2ed1804b7dcd34e7d6514e623ef5c128
yoyo930021 <yoyo930021@gmail.com>
2022-08-02 09:50:27 +0800

    Add exports bug

--- src/app.vue
+++ src/app.vue
@@ -11,35 +11,36 @@
   </f7-app>
 </template>
 <script lang="ts">
-  import { ref, onMounted } from 'vue'
-  import { f7, f7ready, f7App, f7View } from 'framework7-vue'
-  import routes from './routes.js'
+import { ref, onMounted } from 'vue'
+import { f7, f7ready, f7App, f7View } from 'framework7-vue'
+import routes from './routes.js'
+import DatePicker from 'test-a/components/date-picker'
 
-  export default {
-    components: {
-      f7App,
-      f7View
-    },
-    setup() {
+export default {
+  components: {
+    f7App,
+    f7View
+  },
+  setup() {
 
-      // Framework7 Parameters
-      const f7params = {
-        name: 'My App', // App name
-        theme: 'ios', // Automatic theme detection
-        // App routes
-        routes: routes,
-      };
-      onMounted(() => {
-        f7ready(() => {
+    // Framework7 Parameters
+    const f7params = {
+      name: 'My App', // App name
+      theme: 'ios', // Automatic theme detection
+      // App routes
+      routes: routes,
+    };
+    onMounted(() => {
+      f7ready(() => {
 
 
-          // Call F7 APIs here
-        });
+        // Call F7 APIs here
       });
+    });
 
-      return {
-        f7params
-      }
+    return {
+      f7params
     }
   }
+}
 </script>
